6c4cc04b3ad7804592719e81429054acbbe650d3
Tim Oliver <20126140@tafe.wa.edu.au>
2025-12-03 14:02:19 +0800

    feat: add colour scheme for site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@
 module.exports = {
   content: [
     './*.html',
+    './**/.css',
+    './**/.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        polarBlue: {
+          50:  '#E8eaF1',
+          200: '#d1ddec',
+          500: '#5782B7',
+          900: '#0E1620',
+        },
+      },
+    },
   },
   plugins: [],
 }
