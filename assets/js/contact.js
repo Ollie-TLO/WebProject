0af586a813658378d8be4a01b9f8b9d2b81640fa
Tim Oliver <20126140@tafe.wa.edu.au>
2025-12-03 16:38:54 +0800

    fix: add regexp to contact message field

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,4 @@
+// jshint esversion: 6
 // _jshint esversion: 11, strict: implied, undef: true, unused: true, browser: true, devel: true
 
 
@@ -11,39 +12,39 @@ const messageFieldError = document.getElementById('messageFieldError');
 
 const nameFieldRegex = /^[a-zA-Z\s]{2,50}$/;
 const emailFieldRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const messageFieldRegex = /^.{0,1000}$/;
+const messageFieldRegex = /^[a-zA-Z0-9 .,!?;:'"()[\]{}-]{0,1000}$/;
 
 nameField.addEventListener('input', () => {
-	const isValid = nameFieldRegex.test(nameField.value)
-	setErrorState(nameFieldError, isValid)
-})
+	const isValid = nameFieldRegex.test(nameField.value);
+	setErrorState(nameFieldError, isValid);
+});
 
 emailField.addEventListener('input', () => {
-	const isValid = emailFieldRegex.test(emailField.value)
-	setErrorState(emailFieldError, isValid)
-})
+	const isValid = emailFieldRegex.test(emailField.value);
+	setErrorState(emailFieldError, isValid);
+});
 
 messageField.addEventListener('input', () => {
-	const isValid = messageFieldRegex.test(messageField.value)
-	setErrorState(messageFieldError, isValid)
-})
+	const isValid = messageFieldRegex.test(messageField.value);
+	setErrorState(messageFieldError, isValid);
+});
 
 function setErrorState(errorMessageElement, valid) {
 	if (valid) {
-		errorMessageElement.classList.add('hidden')
+		errorMessageElement.classList.add('hidden');
 	} else {
-		errorMessageElement.classList.remove('hidden')
+		errorMessageElement.classList.remove('hidden');
 	}
 }
 
 document.getElementById('contactForm').addEventListener("submit", (event) => {
-	const nameFieldValid = nameFieldRegex.test(nameField.value)
-	const emailFieldValid = emailFieldRegex.test(emailField.value)
-	const messageFieldValid = messageFieldRegex.test(messageField.value)
+	const nameFieldValid = nameFieldRegex.test(nameField.value);
+	const emailFieldValid = emailFieldRegex.test(emailField.value);
+	const messageFieldValid = messageFieldRegex.test(messageField.value);
 
-	setErrorState(nameFieldError, nameFieldValid)
-	setErrorState(emailFieldError, emailFieldValid)
-	setErrorState(messageFieldError, messageFieldValid)
+	setErrorState(nameFieldError, nameFieldValid);
+	setErrorState(emailFieldError, emailFieldValid);
+	setErrorState(messageFieldError, messageFieldValid);
 
 	event.preventDefault(); // prevent page scroller reset
 	if (nameFieldValid && emailFieldValid && messageFieldValid) {
@@ -51,6 +52,6 @@ document.getElementById('contactForm').addEventListener("submit", (event) => {
 	} else {
 		event.preventDefault();  // prevent page scroller reset
 		
-		alert('Please correct the indicated fields')
+		alert('Please correct the indicated fields');
 	}
 });
